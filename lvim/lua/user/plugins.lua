79468940d8baa0eb721836a2110f64528d71e97a
frank <shunhui29@163.com>
2024-10-09 00:10:56 +0800

    add copilot plugin for lvim

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -338,6 +338,18 @@ lvim.plugins = {
             vim.g.mkdp_auto_start = 1
         end,
     },
+
+    {
+        "zbirenbaum/copilot-cmp",
+        event = "InsertEnter",
+        dependencies = { "zbirenbaum/copilot.lua" },
+        config = function()
+            vim.defer_fn(function()
+                require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+                require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+            end, 100)
+        end,
+    },
 }
 
 return M
