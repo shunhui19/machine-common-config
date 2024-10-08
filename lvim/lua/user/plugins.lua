915e73bb0b6cd4fed5817b7e81d00295912e70a3
frank <shunhui29@163.com>
2024-10-08 23:47:05 +0800

    update

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -307,6 +307,37 @@ lvim.plugins = {
             require("colorful-winsep").setup()
         end,
     },
+
+    -- Orgmode
+    {
+        'nvim-orgmode/orgmode',
+        event = 'VeryLazy',
+        ft = { 'org' },
+        config = function()
+            -- Setup orgmode
+            require('orgmode').setup({
+                org_agenda_files = '~/orgfiles/**/*',
+                org_default_notes_file = '~/orgfiles/refile.org',
+            })
+
+            -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+            -- add ~org~ to ignore_install
+            -- require('nvim-treesitter.configs').setup({
+            --   ensure_installed = 'all',
+            --   ignore_install = { 'org' },
+            -- })
+        end,
+    },
+
+    -- Markdown - preview
+    {
+        "iamcco/markdown-preview.nvim",
+        build = "cd app && npm install",
+        ft = "markdown",
+        config = function()
+            vim.g.mkdp_auto_start = 1
+        end,
+    },
 }
 
 return M
